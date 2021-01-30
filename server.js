d3f0fa1b474360751ac9bc0628bfff0777fa9cc1
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-01-30 15:51:59 -0600

    build: Carry out API GET request.

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const app = express();
 
 // ADD DATABASE
 const db = require("./db/db.json");
+
+// SET UP CROSS-ORIGIN RESOURCE SHARING
+// https://medium.com/zero-equals-false/using-cors-in-express-cac7e29b005b
 const cors = require("cors")
 
 // SET UP SERVER TO LISTEN ON PORT 3000
 let port = 3000;
 app.use(cors())
 
+// SET UP API GET (READ) REQUEST
+app.get("/api/notes", (request, respond) => {
+    respond.json(db)
+})
 
 // CONSOLE.LOG MESSAGE TO CONFIRM SERVER IS RUNNING
 app.listen(port,()=>console.log(`Server running on port ${port} with update`))
