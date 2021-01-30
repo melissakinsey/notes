5919b054c8fd91c4614dca6fe428270182f63560
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-01-30 15:09:03 -0600

    build: Set up server.js on port 3000. Install nodemon to refresh server on every file update.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const express = require('express')
+const App = express()
+let port = 3000;
 
 
 
 
+    // / api / notes
 
-/api/notes
+App.listen(port,()=>console.log(`server running on port ${port} with update`))
